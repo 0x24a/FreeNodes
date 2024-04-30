bdc1d3831fe2917208b97b61c46833af9d12283e
0x24a <tanhanze@qq.com>
2024-04-30 19:19:35 +0800

    add free18/v2ray provider

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,9 +1,11 @@
 from providers.chromego import fetch as chromego
 from providers.freefq import fetch as freefq
 from providers.pawdroid import fetch as pawdroid
+from providers.free18 import fetch as free18
 
 PROVIDERS={
     "ChromeGo": chromego,
     "freefq": freefq,
-    "Pawdroid/Free-servers": pawdroid
+    "Pawdroid/Free-servers": pawdroid,
+    "free18/v2ray": free18
 }
---
+++ providers/free18.py
@@ -0,0 +1,2 @@
+from providers.template_b64sub import make_function
+fetch=make_function("https://raw.githubusercontent.com/free18/v2ray/main/v2ray.txt")
