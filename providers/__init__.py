4c114e3ead53826f65dd7acd6ca6bbdd71ef0d66
0x24a <tanhanze@qq.com>
2024-05-02 19:46:01 +0800

    Add 1 provider

--- providers/__init__.py
+++ providers/__init__.py
@@ -3,11 +3,13 @@ from providers.freefq import fetch as freefq
 from providers.pawdroid import fetch as pawdroid
 from providers.free18 import fetch as free18
 from providers.aiboboxx import fetch as aiboboxx
+from providers.hkpc import fetch as hkpc
 
 PROVIDERS={
     "ChromeGo": chromego,
     "freefq": freefq,
     "Pawdroid/Free-servers": pawdroid,
     "free18/v2ray": free18,
-    "aiboboxx": aiboboxx
+    "aiboboxx": aiboboxx,
+    "hkpc/V2ray-Configs": hkpc
 }
---
+++ providers/hkpc.py
@@ -0,0 +1,2 @@
+from providers.template_b64sub import make_function
+fetch=make_function("https://raw.githubusercontent.com/hkpc/V2ray-Configs/main/All_Configs_base64_Sub.txt")
