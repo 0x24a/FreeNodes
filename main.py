fff4ec72309412c3f613b7bd479f9213090bff94
0x24a <tanhanze@qq.com>
2024-05-02 19:56:53 +0800

    update sub updating code; fix hkpc provider

--- main.py
+++ main.py
@@ -19,7 +19,22 @@ print(" - Removing repeated proxies")
 proxies=list(set(proxies))
 print(f" - Number: {number_before} -> {len(proxies)}")
 result = b64encode(("\n".join(proxies)).encode("utf-8")).decode()
+protocols={}
+for proxy in proxies:
+    protocol=proxy.split("://",1)[0]
+    if protocol in protocols.keys():
+        protocols[protocol].append(proxy)
+    else:
+        protocols[protocol]=[proxy]
 print(" - Writing...")
-with open("sub.txt", "w+") as f:
+print(" - - Main sub")
+with open("subs/main.txt", "w+") as f:
     f.write(result)
+for protocol in protocols.items():
+    if not protocol[0] or len(protocol[0]) > 24:
+        continue
+    print(f" - - {protocol[0].upper()}")
+    result = b64encode(("\n".join(protocol[1])).encode("utf-8")).decode()
+    with open(f"subs/{protocol[0].lower()}.txt", "w+") as f:
+        f.write(result)
 print("Done!")
--- providers/hkpc.py
+++ providers/hkpc.py
@@ -1,2 +1,3 @@
 from providers.template_b64sub import make_function
-fetch=make_function("https://raw.githubusercontent.com/hkpc/V2ray-Configs/main/All_Configs_base64_Sub.txt")
+def fetch():
+    return ("\n".join(make_function("https://raw.githubusercontent.com/hkpc/V2ray-Configs/main/All_Configs_base64_Sub.txt")()).split("\n\n")[1]).split("\n")
